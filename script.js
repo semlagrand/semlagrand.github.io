20d7ad84f5de83a811e716d247a3371b3d676759
baeac <baeacbusiness@gmail.com>
2025-02-19 20:32:19 +0100

    Updated projects, models, shaders pages

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function openNav() {
-    document.getElementById("navSidebar").style.width = "240px";
-    document.getElementById("main").style.marginLeft = "240px";
-    document.body.classList.add("sidenav-open");
-}
-
-function closeNav() {
-    document.getElementById("navSidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.classList.remove("sidenav-open");
-}
-
 function scrollToTop() {
     event.preventDefault();
     window.scrollTo({
